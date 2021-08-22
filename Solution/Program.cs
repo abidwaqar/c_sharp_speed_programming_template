08019accee6a44f82816aed66f5421e73bec8208
i160229 Abid Waqar <i160229@nu.edu.pk>
2021-08-22 12:54:09 +0500

    added the template

---
+++ Solution/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Beast_Mode
+{
+    class Program
+    {
+        static dynamic solve()
+        {
+            return "Hello World!!!";
+        }
+
+        static void Main(string[] args)
+        {
+            long T = long.Parse(Console.ReadLine());
+
+            for (int case_number = 1; case_number <= T; ++case_number)
+            { 
+                Console.WriteLine("Case #" + case_number + ": " + solve());
+            }
+        }
+    }
+}
